310e970a6fa38ae30baa85ddc947af634fe24de5
woog <1336026635@qq.com>
2020-01-05 20:55:34 +0800

    code about Networkx

---
+++ Tutorial_Networkx.py
@@ -0,0 +1,45 @@
+import networkx as nx
+
+G = nx.Graph()
+print("G.degree:"+str(G.degree))
+G.add_node(1) #添加一个节点
+print("G.degree:"+str(G.degree))
+G.add_nodes_from([2,3])#add node list
+print("G.degree:"+str(G.degree))
+H = nx.path_graph(20)#线性连接点的路径图
+print("H:"+str(H))
+G.add_nodes_from(H)
+print("G.degree:"+str(G.degree))
+print("G.edges:"+str(G.edges))
+G.add_node(H)
+print("G.degree:"+str(G.degree))
+print("G.edges:"+str(G.edges))
+G.add_edge(1,2)#单独增加一条边
+print("G.edges:"+str(G.edges))
+G.add_edges_from([(1,2),(1,3)])
+print("G.edges:"+str(G.edges))
+print("G.degree:"+str(G.degree))
+print("G.adg:"+str(G.adj))
+print("G.name:"+str(G.name))
+
+G.add_edges_from(H.edges)
+print("H.edges:"+str(H.edges))
+print("H.degree:"+str(H.degree))
+G.clear()
+G.add_edges_from([(1,2),(1,3)])
+G.add_node(1)
+G.add_edge(1,2)
+G.add_node("spam")
+G.add_nodes_from("spam")
+G.add_edge(3,'m')
+list(G.edges)
+print("G.edges"+str(G.edges))
+print(list(G.edges))
+
+G2 = nx.DiGraph(G)
+print("G2.degree:"+list(G2.degree))
+print("G2.edges"+list(G2.edges))
+print("G2.nodes"+list(G2.adg))
+print()
+edgelist= [(0,1),(1,2),(2,3)]
+H = nx.Graph(edgelist)
